9ba4ffb6adc33bc9734f1d033ad5603cd1bbd9da
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2025-04-26 10:51:33 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 const { AuthenticationError, ForbiddenError } = require('../utils/errors');
 
 /**
